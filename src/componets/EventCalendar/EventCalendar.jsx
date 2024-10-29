4dc67c2e620a16025fa67381c5427e7d7ad8e190
alma-assembled <desarrollo@assembled.mx>
2024-10-29 16:03:36 -0600

    recibo maquila

--- src/componets/EventCalendar/EventCalendar.jsx
+++ src/componets/EventCalendar/EventCalendar.jsx
@@ -34,8 +34,8 @@ const EventCalendar = () => {
         tipo: event.tipo,
         op: event.op,
         title: event.titulo,
-        start: new Date(event.fecha_inicio.replace(' ', 'T')),
-        end: new Date(event.fecha_fin.replace(' ', 'T')),
+        start: new Date(event.fecha_inicio),
+        end: new Date(event.fecha_fin),
         description: event.descripcion,
         equipos: event.equipos,
         color: getEventClass(event.tipo)
